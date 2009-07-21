407b8bd5890700809c7dee9a831d93748773ef0f
stefan <stefan@nortd.com>
2009-07-21 15:02:18 +0000

    minor

--- ofxVideoGrabberPtgrey/src/ofxVideoGrabberDc1394.cpp
+++ ofxVideoGrabberPtgrey/src/ofxVideoGrabberDc1394.cpp
@@ -23,7 +23,7 @@ ofxVideoGrabberPtgrey::ofxVideoGrabberPtgrey(){
 	video_mode              = DC1394_VIDEO_MODE_FORMAT7_1;  //2x2 pixel binning
 	color_coding            = DC1394_COLOR_CODING_MONO8;
 	framerate				= DC1394_FRAMERATE_60;
-	
+
 	// In format_7 the framerate is set by setting the packet_size
 	// this is used in dc1394_format7_set_roi()
 	// The following formula is from the libdc1394 faq
@@ -32,13 +32,14 @@ ofxVideoGrabberPtgrey::ofxVideoGrabberPtgrey(){
 	int frame_rate = 60;
 	int depth = 1;
 	int num_packets = (int)(1.0/(bus_period*frame_rate) + 0.5);
-	packet_size =  (width*height*depth + (num_packets*8) - 1)/(num_packets*8);
-	
+	//packet_size =  (width*height*depth + (num_packets*8) - 1)/(num_packets*8);
+	packet_size = DC1394_USE_MAX_AVAIL;
+
     driver = dc1394_new();
     if (!driver) { return; }
     err = dc1394_camera_enumerate(driver, &deviceList);
 	if(err){ ofLog(OF_LOG_ERROR, "Failed to enumerate cameras"); }
-	
+
     if( deviceList->num == 0 ) {
 		ofLog(OF_LOG_ERROR, "No cameras found");
     }
@@ -49,10 +50,10 @@ ofxVideoGrabberPtgrey::ofxVideoGrabberPtgrey(){
 //--------------------------------------------------------------------
 ofxVideoGrabberPtgrey::~ofxVideoGrabberPtgrey(){
 	close();
-    
+
     //free device enumeration
     dc1394_camera_free_list(deviceList);
-    
+
     //free context
     dc1394_free(driver);
 
@@ -62,13 +63,13 @@ ofxVideoGrabberPtgrey::~ofxVideoGrabberPtgrey(){
 //--------------------------------------------------------------------
 void ofxVideoGrabberPtgrey::listDevices(){
     ofLog(OF_LOG_NOTICE, "-------------------------------------");
-        
+
 	ofLog(OF_LOG_NOTICE, "Listing available IIDC capture devices.\nUse unit number as unique ID for camera...");
 	for (uint32_t index = 0; index < deviceList->num; index++) {
 		//printf("Using camera with GUID %"PRIx64"\n", camera->guid);
 		ofLog(OF_LOG_NOTICE, "Device[%d].unit = %x, GUID = %x", index, deviceList->ids[index].unit, deviceList->ids[index].guid);
-	}    
-    
+	}
+
     ofLog(OF_LOG_NOTICE, "-------------------------------------");
 }
 
@@ -116,30 +117,30 @@ void ofxVideoGrabberPtgrey::grabFrame(){
     if (camera != NULL) {
 		// get a frame
         // The first time you call a DMA capture function dc1394_capture_dequeue() it returns
-		// a pointer to the first frame buffer structure (dc1394frame_t). After a successful 
-		// capture function call, the capture_buffer pointer and the frame buffer it points 
-		// to are available to you for reading and writing. It will not be overwritten with 
-		// a newer frame while it is allocated to you (FREE), even if the ring buffer overflows. 
-		// Once you have finished with it you should release it as soon as possible with a call 
+		// a pointer to the first frame buffer structure (dc1394frame_t). After a successful
+		// capture function call, the capture_buffer pointer and the frame buffer it points
+		// to are available to you for reading and writing. It will not be overwritten with
+		// a newer frame while it is allocated to you (FREE), even if the ring buffer overflows.
+		// Once you have finished with it you should release it as soon as possible with a call
 		// to dc1394_capture_enqueue().
 		err = dc1394_capture_dequeue(camera, DC1394_CAPTURE_POLICY_POLL, &frame);
         if (frame == NULL) {
             bIsFrameNew = false;
         } else {
 			bIsFrameNew = true;
-			
+
 			// copy into pixels
 			for( int i = 0; i < height; i++ ) {
 				memcpy( pixels + (i*width), frame->image + (i*frame->stride), width );
-			}		
+			}
 
 			if (bUseTexture) {
 				tex.loadData(frame->image, width, height, GL_LUMINANCE);
 			}
-			
-			// make frame available again as part of the 
+
+			// make frame available again as part of the
 			// ring buffer receiving images from the cam
-			err = dc1394_capture_enqueue(camera, frame);		
+			err = dc1394_capture_enqueue(camera, frame);
 		}
 	}
 }
@@ -173,7 +174,7 @@ bool ofxVideoGrabberPtgrey::initGrabber(int w, int h, bool setUseTexture){
     width = w;
     height = h;
     bUseTexture = setUseTexture;
-	
+
     //first choose a device to use
     if( bChooseDevice ) {
         bool bFound = false;
@@ -192,38 +193,35 @@ bool ofxVideoGrabberPtgrey::initGrabber(int w, int h, bool setUseTexture){
     } else {
         ofLog(OF_LOG_ERROR, "in initGrabber, No cameras found");
     }
-	
-    	
+
+
     camera = dc1394_camera_new(driver, deviceID);
-	if (!camera) { 
+	if (!camera) {
 		ofLog(OF_LOG_ERROR, "Failed to initialize camera with guid %x", deviceID);
 		return 1;
 	}
-	
+
     /*-----------------------------------------------------------------------
      *  setup capture
      *-----------------------------------------------------------------------*/
-	
+
     err = dc1394_video_set_iso_speed(camera, DC1394_ISO_SPEED_400);
     //DC1394_ERR_CLN_RTN(err,cleanup_and_exit(camera),"Could not set iso speed");
-	
 
-
-	
 	//err = dc1394_format7_set_color_coding(camera, video_mode, color_coding);
 	//DC1394_ERR_CLN_RTN(err,cleanup_and_exit(camera),"Could not set color coding");
-	err = dc1394_format7_set_roi(camera, video_mode, color_coding, packet_size, 0,0, 640,480);
+	err = dc1394_format7_set_roi(camera, video_mode, color_coding, packet_size, 0,0, width,height);
 	//DC1394_ERR_CLN_RTN(err,cleanup_and_exit(camera),"Could not set format7");
-	
+
 	//err = dc1394_video_set_mode(camera, video_mode);
 	//DC1394_ERR_CLN_RTN(err,cleanup_and_exit(camera),"Could not set video mode");
-	
+
     err = dc1394_video_set_framerate(camera, framerate);
 	//DC1394_ERR_CLN_RTN(err,cleanup_and_exit(camera),"Could not set framerate");
-	
+
     err = dc1394_capture_setup(camera,2, DC1394_CAPTURE_FLAGS_DEFAULT);
     //DC1394_ERR_CLN_RTN(err,cleanup_and_exit(camera),"Could not setup camera-\nmake sure that the video mode and framerate are\nsupported by your camera");
-	
+
 
 	/*-----------------------------------------------------------------------
      *  set camera's features
@@ -237,14 +235,14 @@ bool ofxVideoGrabberPtgrey::initGrabber(int w, int h, bool setUseTexture){
 
 	err = dc1394_feature_set_mode(camera, DC1394_FEATURE_SHUTTER, DC1394_FEATURE_MODE_MANUAL);
 	err = dc1394_feature_set_value(camera, DC1394_FEATURE_SHUTTER, 60);  //1-263, 64 being the max for 60fps
-	
+
 	err = dc1394_feature_set_mode(camera, DC1394_FEATURE_GAIN, DC1394_FEATURE_MODE_MANUAL);
 	err = dc1394_feature_set_value(camera, DC1394_FEATURE_GAIN, 50);  //16-64
 
 	//err = dc1394_feature_set_mode(camera, DC1394_FEATURE_PAN, DC1394_FEATURE_MODE_MANUAL);
 	//err = dc1394_feature_set_value(camera, DC1394_FEATURE_PAN, 56);
-	
-	
+
+
     /*-----------------------------------------------------------------------
      *  report camera's features
      *-----------------------------------------------------------------------*/
@@ -255,23 +253,23 @@ bool ofxVideoGrabberPtgrey::initGrabber(int w, int h, bool setUseTexture){
     else {
         dc1394_feature_print_all(&features, stdout);
     }
-	
+
     /*-----------------------------------------------------------------------
      *  have the camera start sending us data
      *-----------------------------------------------------------------------*/
     err=dc1394_video_set_transmission(camera, DC1394_ON);
     //DC1394_ERR_CLN_RTN(err,cleanup_and_exit(camera),"Could not start camera iso transmission");
-	
+
 	//create pixel buffer
     pixels	= new unsigned char[width * height];
-    
+
     //create texture
     if (bUseTexture) {
         tex.allocate(width,height,GL_LUMINANCE);
         memset(pixels, 0, width*height);
         tex.loadData(pixels, width, height, GL_LUMINANCE);
     }
-	
+
 }
 
 
